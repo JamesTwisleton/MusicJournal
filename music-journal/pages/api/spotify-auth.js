ffeac620cbb915ab1ec64712ea30d37b1ebe216f
Twisleton <ajtwisleton@gmail.com>
2020-09-19 11:23:44 +0100

    auth starting to work

--- music-journal/pages/api/spotify-auth.js
+++ music-journal/pages/api/spotify-auth.js
@@ -1,10 +1,40 @@
 const firebaseAdmin = require('firebase-admin');
-firebaseAdmin.initializeApp();
+const serviceAccount = require('../../service-account.json');
+const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
 
-//Should be able to use service account (messaged securely) and this guide
-https://github.com/firebase/functions-samples/blob/master/spotify-auth/functions/index.js
+if(!firebaseAdmin.apps.length) {
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+}
+
+
+
+// Spotify OAuth 2 setup
+// TODO: Configure the `spotify.client_id` and `spotify.client_secret` Google Cloud environment variables.
+const SpotifyWebApi = require('spotify-web-api-node');
+const Spotify = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.FIREBASE_REDIRECT_URL,
+});
+
+// Scopes to request.
+const OAUTH_SCOPES = ['user-read-email'];
+
 
 export default function handler(req, res) {
-    console.log(firebaseAdmin);
-    res.status(200).json('wow');
+  
+    const state = req.cookies.state || crypto.randomBytes(20).toString('hex');
+    // console.log('Setting verification state:', state);
+    // res.cookie('state', state.toString(), {maxAge: 3600000, secure: true, httpOnly: true});
+    const authorizeURL = Spotify.createAuthorizeURL(OAUTH_SCOPES, state.toString());
+    res.redirect(authorizeURL);
+  
   }
+
+
+//Should be able to use service account (messaged securely) and this guide
+//https://github.com/firebase/functions-samples/blob/master/spotify-auth/functions/index.js
