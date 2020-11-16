c8956b7459101fd8d7189cbdfb20955175801618
James Twisleton <ajtwisleton@connect.ust.hk>
2020-11-16 17:14:15 +0000

    added cors

--- music-journal/pages/api/spotify-auth.js
+++ music-journal/pages/api/spotify-auth.js
@@ -3,8 +3,30 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const firebaseAdmin = require('firebase-admin');
 const serviceAccount = require('../../service-account.json');
 const Cookies = require('cookies');
+import Cors from 'cors';
 
-const handler = (req, res) => {
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+
+async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+
   if (!firebaseAdmin.apps.length) {
     firebaseAdmin.initializeApp({
       credential: firebaseAdmin.credential.cert(serviceAccount),
@@ -17,7 +39,7 @@ const handler = (req, res) => {
     redirectUri: process.env.AUTH_REDIRECT_URL,
   });
   const OAUTH_SCOPES = ['user-read-email', 'user-read-recently-played'];
-  
+
   return redirectToSpotify(req, res, Spotify, OAUTH_SCOPES);
 };
 
--- music-journal/pages/api/spotify-redirect.js
+++ music-journal/pages/api/spotify-redirect.js
@@ -2,7 +2,27 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const firebaseAdmin = require('firebase-admin');
 const serviceAccount = require('../../service-account.json');
 const Cookies = require('cookies');
-const handler = (req, res) => {
+import Cors from 'cors';
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   if (!firebaseAdmin.apps.length) {
     firebaseAdmin.initializeApp({
       credential: firebaseAdmin.credential.cert(serviceAccount),
