23c578840e4e03e3acbf2135b3ba05766c0b4b89
= <ajtwisleton@gmail.com>
2020-11-16 22:11:32 +0000

    slight refactor

--- music-journal/pages/api/spotify-auth.js
+++ music-journal/pages/api/spotify-auth.js
@@ -4,10 +4,22 @@ const firebaseAdmin = require('firebase-admin');
 const serviceAccount = require('../../service-account.json');
 const Cookies = require('cookies');
 import Cors from 'cors';
-
 const cors = Cors({
   methods: ['GET', 'HEAD'],
 });
+const OAUTH_SCOPES = ['user-read-email', 'user-read-recently-played'];
+if (!firebaseAdmin.apps.length) {
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+}
+const Spotify = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.AUTH_REDIRECT_URL,
+});
+
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
@@ -26,24 +38,6 @@ function runMiddleware(req, res, fn) {
 
 async function handler(req, res) {
   await runMiddleware(req, res, cors);
-
-  if (!firebaseAdmin.apps.length) {
-    firebaseAdmin.initializeApp({
-      credential: firebaseAdmin.credential.cert(serviceAccount),
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-    });
-  }
-  const Spotify = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: process.env.AUTH_REDIRECT_URL,
-  });
-  const OAUTH_SCOPES = ['user-read-email', 'user-read-recently-played'];
-
-  return redirectToSpotify(req, res, Spotify, OAUTH_SCOPES);
-};
-
-async function redirectToSpotify(req, res, Spotify, OAUTH_SCOPES) {
   const cookies = new Cookies(req, res);
   const state = req.cookies.state || crypto.randomBytes(20).toString('hex');
   cookies.set('verificationState', state.toString());
@@ -56,6 +50,8 @@ async function redirectToSpotify(req, res, Spotify, OAUTH_SCOPES) {
   });
 
   return res.end();
-}
+
+};
+
 
 export default handler;
